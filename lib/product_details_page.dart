ae239d0f80a75d36573a9d7c98832205dfdce533
cnewtonharris <col1695@hotmail.com>
2025-01-18 19:11:40 +0000

    added sizing options to the bottom of the product page

--- lib/product_details_page.dart
+++ lib/product_details_page.dart
@@ -15,16 +15,49 @@ class ProductDetailsPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text(product['title']as String,
+          Text(
+            product['title'] as String,
             style: Theme.of(context).textTheme.titleLarge,
           ),
           const Spacer(),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Image.asset(product['image']as String, height: 300,
+            child: Image.asset(
+              product['image'] as String,
+              height: 300,
             ),
           ),
-          const Spacer(flex: 2),
+          const Spacer(),
+          Container(
+            //height: 50,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(245, 247, 249, 1),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  '\$${product['price']}',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: (product['size'] as List<String>).length,
+                    itemBuilder: (context, index) {
+                      final size = (product['size'] as List<String>)[index];
+
+                      return Chip(
+                        label: Text(size),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
