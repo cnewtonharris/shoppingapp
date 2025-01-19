05e942eb85d80bf6b7fd1d72bef52e5a4317482f
cnewtonharris <col1695@hotmail.com>
2025-01-19 18:55:30 +0000

    added "add to cart" button, improved ui for sizing options, added elevated button and backgrounds to each button

--- lib/product_details_page.dart
+++ lib/product_details_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ProductDetailsPage extends StatelessWidget {
+class ProductDetailsPage extends StatefulWidget {
   final Map<String, Object> product;
   const ProductDetailsPage({
     super.key,
     required this.product,
   });
 
+  @override
+  State<ProductDetailsPage> createState() => _ProductDetailsPageState();
+}
+
+class _ProductDetailsPageState extends State<ProductDetailsPage> {
+  String selectedSize = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +22,14 @@ class ProductDetailsPage extends StatelessWidget {
       body: Column(
         children: [
           Text(
-            product['title'] as String,
+            widget.product['title'] as String,
             style: Theme.of(context).textTheme.titleLarge,
           ),
           const Spacer(),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Image.asset(
-              product['image'] as String,
+              widget.product['image'] as String,
               height: 300,
             ),
           ),
@@ -35,9 +41,10 @@ class ProductDetailsPage extends StatelessWidget {
               borderRadius: BorderRadius.circular(40),
             ),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  '\$${product['price']}',
+                  '\$${widget.product['price']}',
                   style: Theme.of(context).textTheme.titleLarge,
                 ),
                 const SizedBox(height: 10),
@@ -45,14 +52,46 @@ class ProductDetailsPage extends StatelessWidget {
                   height: 50,
                   child: ListView.builder(
                     scrollDirection: Axis.horizontal,
-                    itemCount: (product['size'] as List<String>).length,
+                    itemCount: (widget.product['size'] as List<String>).length,
                     itemBuilder: (context, index) {
-                      final size = (product['size'] as List<String>)[index];
+                      final size = (widget.product['size'] as List<String>)[index];
 
-                      return Chip(
-                        label: Text(size),
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              selectedSize = size;
+                            });
+                          },
+                          child: Chip(
+                            label: Text(size),
+                            backgroundColor: selectedSize == size
+                                ? Theme.of(context).colorScheme.primary
+                                : null,
+                          ),
+                        ),
                       );
                     },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ElevatedButton(
+                      onPressed: (){},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                      child: const Text('Add to Cart',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      )
                   ),
                 ),
               ],
