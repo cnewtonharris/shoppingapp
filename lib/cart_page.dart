be9f45ab4163d7f6adf81b22e464b05e53b976b4
cnewtonharris <col1695@hotmail.com>
2025-01-21 06:31:53 +0000

    cart ui updated, image and title added

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingapp/global_variables.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Cart Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: ListView.builder(
+          itemCount: cart.length,
+          itemBuilder: (context, index){
+            final cartItem = cart[index];
+
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(cartItem['image'].toString()),
+                radius: 30,
+              ),
+              title: Text(
+                  cartItem['title'].toString()
+              ),
+              subtitle: Text('Size: ${cartItem['size']}' + '\n' + 'Price: \$${cartItem['price']}'),
+            );
+        },
       ),
     );
   }
